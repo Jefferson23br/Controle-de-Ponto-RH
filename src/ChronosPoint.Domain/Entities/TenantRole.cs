dc95b3376f39650eddd90e104108515fc61e56dd
Jefferson Luis de Lima <126838214+Jefferson23br@users.noreply.github.com>
2026-05-14 13:25:50 -0300

    Create TenantRole.cs

---
+++ src/ChronosPoint.Domain/Entities/TenantRole.cs
@@ -0,0 +1,14 @@
+// -----------------------------------------------------------------------------
+// Copyright (C) 2026 ChronosPoint. Todos os direitos reservados.
+// Este codigo e de propriedade exclusiva e confidencial.
+// A reproducao ou distribuicao sem autorizacao previa por escrito e proibida.
+// -----------------------------------------------------------------------------
+
+namespace ChronosPoint.Domain.Entities;
+
+public enum TenantRole
+{
+    User = 0,
+    Manager = 1,
+    Admin = 2
+}
